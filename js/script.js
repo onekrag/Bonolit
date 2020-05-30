60a5ad9d8938307482d73935bb4cd22a9cea3c40
Andrey <rumpel45@ya.ru>
2020-05-30 17:16:56 +0500

    12

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
+        $('body').toggleClass('lock');
+      });
+      
+            
+      $('.header__menu').click(function (event) {
+        $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
+        $('body').toggleClass('lock');
+      });  
+
+});
