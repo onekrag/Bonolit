fcd61fa6c4cc0ca7f7da31396a5c3c5fd905d577
Andrey <rumpel45@ya.ru>
2020-05-31 09:10:17 +0500

    4section

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
 
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
-        $('body').toggleClass('lock');
-      });
-      
-            
-      $('.header__menu').click(function (event) {
-        $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
-        $('body').toggleClass('lock');
-      });  
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
 
-      $(".owl-carousel").owlCarousel({
-        autoWidth: true,
-        nav: true,
-        loop: true,
-      });
+
+  $('.header__menu').click(function (event) {
+    $('.header__burger,.header__menu,.header__phone,.header__email').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  $(".slide__clients").owlCarousel({
+    autoWidth: true,
+    nav: true,
+    loop: true,
+    dots: false,
+    margin: 50,
+  });
+  $(".slide__reviews").owlCarousel({
+    autoWidth: true,
+    nav: true,
+    loop: true,
+    dots: false,
+    margin: 50,
+  });
 
 });
